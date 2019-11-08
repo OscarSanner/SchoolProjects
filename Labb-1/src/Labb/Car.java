328198c46985aabc459b065a465ea3d95f9f6189
OlofSjogren <olofsjogren97@gmail.com>
2019-11-08 18:50:47 +0100

    Half of documentation to be merged with Oscars half, TODO merge

--- Labb-1/src/Labb/Car.java
+++ Labb-1/src/Labb/Car.java
@@ -2,6 +2,11 @@ package Labb;
 
 import java.awt.*;
 
+/**
+ * Abstract class for all cars.
+ * Contains methods for moving a car in 2d.
+ * Contains common properties of a car.
+ */
 public abstract class Car implements Movable {
     private static Direction[] directionArrayRight = {Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT};
     private static Direction[] directionArrayLeft = {Direction.LEFT, Direction.DOWN, Direction.RIGHT, Direction.UP};
@@ -21,6 +26,13 @@ public abstract class Car implements Movable {
     // Metoden move ska fungera så att beroende på riktning ökas (eller minskas)
     // bilens x- eller y-koordinat med dess currentSpeed.
 
+    /**
+     * Constructor for common properites of a car, all cars initiated standing still.
+     * @param nrDoors the number of doors on the car.
+     * @param color the color of the car.
+     * @param enginePower the driving power of the car, max speed.
+     * @param modelName the cars model.
+     */
     public Car(int nrDoors, Color color, double enginePower, String modelName) {
         this.nrDoors = nrDoors;
         this.color = color;
@@ -34,55 +46,109 @@ public abstract class Car implements Movable {
         stopEngine();
     }
 
+    /**
+     * Method for getting the current direction the car is facing in a 2d plane.
+     * @return the direction the car is facing.
+     */
     public Direction getCurrentDirection() {
         return currentDirection;
     }
 
+    /**
+     * Method for getting the current x-coordinate of the car.
+     * @return the x-coordinate.
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Method for getting the current y-coordinate of the car.
+     * @return the y-coordinate.
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Sets the current speed of the car to the given value.
+     * Used as a helper method for starting the car, acceleration and braking.
+     * @param currentSpeed the given value.
+     */
     private void setCurrentSpeed(double currentSpeed) {
         this.currentSpeed = currentSpeed;
     }
 
+    /**
+     * Stops the engine of the car, setting its speed to 0.
+     */
     public void stopEngine() {
         currentSpeed = 0;
     }
 
+    /**
+     * Method for getting the numbers of door on the car.
+     * @return the number of doors on the car.
+     */
     public int getNrDoors() {
         return nrDoors;
     }
 
+    /**
+     * Method for getting the driving power of the car, max speed.
+     * @return the maximum speed.
+     */
     public double getEnginePower() {
         return getEnginePower;
     }
 
+    /**
+     * Method for getting the current speed of the car.
+     * @return the current speed.
+     */
     public double getCurrentSpeed() {
         return currentSpeed;
     }
 
+    /**
+     * Method for getting the color of the car.
+     * @return the color of the car.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Method for setting the color of the car to the given color.
+     * @param clr the given color the car will get.
+     */
     public void setColor(Color clr) {
         color = clr;
     }
 
+    /**
+     * Method for starting the car by giving it a starting speed.
+     */
     public void startEngine() {
         currentSpeed = 0.1;
     }
 
-
+    /**
+     * Increases the speed of the car with the given amount from the gas-method.
+     * The increase of speed is determined by adding a speedfactor (in conjuction with the amount) to the current speed.
+     * The speedfactor is calculated through another method, specific for each type of car.
+     * @param amount how much the cars speed will increase, given by gas-method.
+     */
     private void incrementSpeed(double amount) {
         setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount, getEnginePower()));
     }
 
+    /**
+     * Decreases the speed of the car with the given amount from the brake-method.
+     * The decrease of speed is determined by subtracting a speedfactor (in conjuction with the amount) to the current speed.
+     * The speedfactor is calculated through another method, specific for each type of car.
+     * @param amount how much the cars speed will decrease, given by brake-method.
+     */
     private void decrementSpeed(double amount) {
         setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount, 0));
     }
